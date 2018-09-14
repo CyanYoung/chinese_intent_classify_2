42976a9cef52a2e9728953c0f8ee469db352c66f
yangjc <bupt_yjc@163.com>
2018-09-14 16:55:57 +0800

    change nn param

--- nn_arch.py
+++ nn_arch.py
@@ -6,20 +6,20 @@ import keras.backend as K
 
 
 def dnn(embed_input, class_num):
-    da1 = Dense(300, activation='relu')
-    da2 = Dense(300, activation='relu')
+    da1 = Dense(200, activation='relu')
+    da2 = Dense(200, activation='relu')
     da3 = Dense(class_num, activation='softmax')
     x = Lambda(lambda a: K.mean(a, axis=1))(embed_input)
     x = da1(x)
     x = da2(x)
-    x = Dropout(0.2)(x)
+    x = Dropout(0.5)(x)
     return da3(x)
 
 
 def cnn(embed_input, class_num):
-    ca1 = Conv1D(filters=32, kernel_size=1, padding='same', activation='relu')
-    ca2 = Conv1D(filters=32, kernel_size=2, padding='same', activation='relu')
-    ca3 = Conv1D(filters=32, kernel_size=3, padding='same', activation='relu')
+    ca1 = Conv1D(filters=64, kernel_size=1, padding='same', activation='relu')
+    ca2 = Conv1D(filters=64, kernel_size=2, padding='same', activation='relu')
+    ca3 = Conv1D(filters=64, kernel_size=3, padding='same', activation='relu')
     da = Dense(class_num, activation='softmax')
     x1 = ca1(embed_input)
     x1 = BatchNormalization()(x1)
@@ -31,14 +31,14 @@ def cnn(embed_input, class_num):
     x3 = BatchNormalization()(x3)
     x3 = GlobalMaxPooling1D()(x3)
     x = Concatenate()([x1, x2, x3])
-    x = Dropout(0.2)(x)
+    x = Dropout(0.5)(x)
     return da(x)
 
 
 def rnn(embed_input, class_num):
-    ra = LSTM(300, activation='tanh')
+    ra = LSTM(200, activation='tanh')
     da = Dense(class_num, activation='softmax')
     x = Masking()(embed_input)
     x = ra(x)
-    x = Dropout(0.2)(x)
+    x = Dropout(0.5)(x)
     return da(x)
