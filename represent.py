9a11f20e181fb4fca05c174125ede810219984d8
yangjc <bupt_yjc@163.com>
2018-10-15 10:46:12 +0800

    simplify param

--- represent.py
+++ represent.py
@@ -47,7 +47,7 @@ def label2ind(labels, path_label_ind):
         pk.dump(label_inds, f)
 
 
-def align(sents, labels, path_word2ind, path_sent, path_label, mode):
+def align(sents, labels, path_sent, path_label, mode):
     with open(path_word2ind, 'rb') as f:
         model = pk.load(f)
     seqs = model.texts_to_sequences(sents)
@@ -72,7 +72,7 @@ def vectorize(path_data, path_sent, path_label, mode):
     if mode == 'train':
         embed(sents, path_word2ind, path_word_vec, path_embed)
         label2ind(labels, path_label_ind)
-    align(sents, labels, path_word2ind, path_sent, path_label, mode)
+    align(sents, labels, path_sent, path_label, mode)
 
 
 if __name__ == '__main__':
