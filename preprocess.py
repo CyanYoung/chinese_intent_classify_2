0a40cd1b2a3f3facd0fc1c49a13ca487aa328a2c
yangjc <bupt_yjc@163.com>
2018-12-06 11:00:21 +0800

    simplify format

--- preprocess.py
+++ preprocess.py
@@ -27,8 +27,7 @@ def save(path, texts, labels):
 
 def prepare(path_univ_dir, path_train, path_test):
     text_set = set()
-    texts = list()
-    labels = list()
+    texts, labels = list(), list()
     files = os.listdir(path_univ_dir)
     for file in files:
         label = os.path.splitext(file)[0]
