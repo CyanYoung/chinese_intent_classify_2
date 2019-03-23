1081e2cae104c078a0f92e9391dc256a17882c82
yangjc <bupt_yjc@163.com>
2019-03-23 11:14:48 +0800

    add f1

--- eval.py
+++ eval.py
@@ -2,7 +2,7 @@ import pickle as pk
 
 import numpy as np
 
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 from classify import ind_labels, models
 
@@ -18,12 +18,25 @@ with open(path_sent, 'rb') as f:
 with open(path_label, 'rb') as f:
     labels = pk.load(f)
 
+class_num = len(ind_labels)
+
+paths = {'adnn': 'stat/metric/adnn.csv',
+         'crnn': 'stat/metric/crnn.csv',
+         'rcnn': 'stat/metric/rcnn.csv'}
+
 
 def test(name, sents, labels):
     model = map_item(name, models)
     probs = model.predict(sents)
     preds = np.argmax(probs, axis=1)
-    print('\n%s %s %.2f\n' % (name, 'acc:', accuracy_score(labels, preds)))
+    precs = precision_score(labels, preds, average=None)
+    recs = recall_score(labels, preds, average=None)
+    with open(map_item(name, paths), 'w') as f:
+        f.write('label,prec,rec' + '\n')
+        for i in range(class_num):
+            f.write('%s,%.2f,%.2f\n' % (ind_labels[i], precs[i], recs[i]))
+    f1 = f1_score(labels, preds, average='weighted')
+    print('\n%s f1: %.2f - acc: %.2f\n' % (name, f1, accuracy_score(labels, preds)))
     for text, label, pred in zip(texts, labels, preds):
         if label != pred:
             print('{}: {} -> {}'.format(text, ind_labels[label], ind_labels[pred]))
